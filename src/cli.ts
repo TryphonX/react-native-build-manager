1739df7cfaef2fbe6792e0f2349f26e7309f86d9
Tryphon Ksydas <tryphon.ksydas@gmail.com>
2021-08-01 01:44:36 +0300

    feat: special message when manually exiting

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,15 @@
 #! /usr/bin/env node
 import { initAsync } from './index.js';
+import chalk from 'chalk';
+
+const { red } = chalk;
+
+process.on('uncaughtException', () => {
+	
+	//error(err.message);
+
+	console.log(`${red.bold('!')} Aborted!\n`);
+	process.exit();
+});
 
 initAsync();
